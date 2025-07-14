97d5f11ff0ef83b35a594a5ef7347e106c718890
jamesrulz <jamesmilo42@gmail.com>
2025-07-15 09:01:17 +1000

    WIP

--- merkle_tree.c
+++ merkle_tree.c
@@ -5,10 +5,26 @@
 /*
 TODO -- stopped at working on how to update this to take in variable data sizes
 
+// save back into memory
+// 32 leaf nodes > 1 32*32 > 32 nodes hsah back 1
+
+// Read up to block_size * leaf_block_count data into a buffer from the file
+// Hash that using merkle tree structure to produce a single 224 string
+// Append that to 
+
+32 * 32 -> 1
+32 * 32 -> 2
+32 * 32 * 32 > 1
+Maximum possible hash level --> choose a number --> store this in Bram
+
+Stream in data into the leaf nodes
+ >> load file into ram first --> loading from disk very slow
+ >> data stream into leaf nodes at some rate (as fast as we can -- this is likely to be limiting factor instead of the compute). Probably max is 56 bytes per cycle but unlikely to get there with streaming.
 */
 
 /* Start with the assumption that we have 32 blocks worth of data */
 // TODO - replace divides with shifts
+// TODO - seperate write/read buffers
 #define BLOCKS 32
 #define DATA_IN_BYTES 1792
 
