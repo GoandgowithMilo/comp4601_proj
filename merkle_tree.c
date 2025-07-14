4453970273aa12867ac52f5d532a5f90fa9ca682
jamesrulz <jamesmilo42@gmail.com>
2025-07-14 10:30:21 +1000

    WIP adding ability to absorb variable data sizes

--- merkle_tree.c
+++ merkle_tree.c
@@ -1,32 +1,11 @@
-// typedef struct { 
-//     char *hashed_data;
-//     char *raw_data;
-// } merkle_tree_node;
-
-/*
-    > Read data from a file into an array.
-start:
-    > Read up to 16 blocks of data from that array of size 448 bits (56 bytes)
-    > iterate through each block, hashing it
-    > Store these hashed blocks in result array
-    > Go back to start until whole file has been processed and stored in result array
-
-DFS MIGHT BE EASIEST HERE
-    > Read in data from a file into an array
-Retrieve:
-    > Retrieve up to 16 blocks worth of data from the array
-    > Hash each of those blocks 
-    > Combine pairs and has those 8 blocks
-    > Combine pairs and hash those 4 blocks
-    > Combine pairs and hash those 2 blocks
-    > Store the last block in a results array
-    > If data from file still hasn't been processed, repeat
-
-*/
-
 #include <stdio.h>
 #include <assert.h>
 #include "sha3_224.c"
+
+/*
+TODO -- stopped at working on how to update this to take in variable data sizes
+
+*/
 
 /* Start with the assumption that we have 32 blocks worth of data */
 // TODO - replace divides with shifts
