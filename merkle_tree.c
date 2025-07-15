ced01a3173d419001a12575072fadf16f7877633
jamesrulz <jamesmilo42@gmail.com>
2025-07-15 12:05:25 +1000

    Updated code to handle variable data size and block count

--- merkle_tree.c
+++ merkle_tree.c
@@ -1,63 +1,42 @@
 #include <stdio.h>
-#include <assert.h>
 #include "sha3_224.c"
 
-/*
-TODO -- stopped at working on how to update this to take in variable data sizes
-
-// save back into memory
-// 32 leaf nodes > 1 32*32 > 32 nodes hsah back 1
-
-// Read up to block_size * leaf_block_count data into a buffer from the file
-// Hash that using merkle tree structure to produce a single 224 string
-// Append that to 
-
-32 * 32 -> 1
-32 * 32 -> 2
-32 * 32 * 32 > 1
-Maximum possible hash level --> choose a number --> store this in Bram
-
-Stream in data into the leaf nodes
- >> load file into ram first --> loading from disk very slow
- >> data stream into leaf nodes at some rate (as fast as we can -- this is likely to be limiting factor instead of the compute). Probably max is 56 bytes per cycle but unlikely to get there with streaming.
-*/
-
-/* Start with the assumption that we have 32 blocks worth of data */
-// TODO - replace divides with shifts
-// TODO - seperate write/read buffers
-#define BLOCKS 32
-#define DATA_IN_BYTES 1792
+#define DATA_SIZE 1433600000 // ~1.4Gb - this is 1000000 sets of 32 blocks
+// #define DATA_SIZE 1433600 // ~1.4Mb - this is 100 sets of 32 blocks
+// #define DATA_SIZE 1792 // this fits perfectly into 32 blocks
+#define LEAF_COUNT 32
+// #define LEAF_COUNT 16
+// #define LEAF_COUNT 8
+#define BLOCK_SIZE 56 // each block stores up to 448 bits (56 bytes)
 
 int main() {
-    const unsigned char data_to_hash[DATA_IN_BYTES] = {0}; // unhashed data
-    unsigned char hashed_data[DATA_IN_BYTES / 2] = {0}; // each hash takes 56 bytes and produces 28 so at most this only needs to be half as big in this implementation
-    int block_size_in_bytes = DATA_IN_BYTES / BLOCKS;
-    int blocks_to_hash = BLOCKS;
+    static unsigned char raw_data[DATA_SIZE] = {0};
+    raw_data[DATA_SIZE - 1] = 2; // sentinel value to check we're hashing everything
+    static unsigned char hashed_data[DATA_SIZE/2];
+    unsigned long int data_processed = 0;
 
-    assert(block_size_in_bytes == 56); // this should always be 56 (448 bits) for our purposes
+    // Read in all the data to leaf nodes and hash them
+    while (data_processed != DATA_SIZE) {
+        for (int i = 0; i < LEAF_COUNT; i++) {
+            SHA3_224(&raw_data[data_processed + i * BLOCK_SIZE], &hashed_data[data_processed/2 + i * BLOCK_SIZE/2]);
+        }
 
-    // Hash all the lower blocks
-    for (int i = 0; i < blocks_to_hash; i++) { // for each of the data blocks
-        SHA3_224(&data_to_hash[i*block_size_in_bytes], &hashed_data[i*(block_size_in_bytes/2)]);
+        data_processed += (LEAF_COUNT * BLOCK_SIZE);
     }
 
-    // // Test printing
-    // for (int i = 0; i < blocks_to_hash; i++) {
-    //     for (int j = 0; j < block_size_in_bytes/2; j++) {
-    //         printf("%02x", hashed_data[(i * block_size_in_bytes/2) + j]);
-    //     }
-
-    //     printf("\n");
-    // }
-
-    while ((blocks_to_hash = blocks_to_hash/2) != 1) {
-        for (int i = 0; i < blocks_to_hash; i++) {
-            SHA3_224(&hashed_data[i*block_size_in_bytes], &hashed_data[i*(block_size_in_bytes/2)]);
+    // We now have to pair adjacent nodes and hash them
+    int blocks_to_process = (data_processed / BLOCK_SIZE) / 2;
+    
+    while (blocks_to_process != 0) {
+        for (int i = 0; i < blocks_to_process; i++) {
+            SHA3_224(&hashed_data[i*BLOCK_SIZE], &hashed_data[i*(BLOCK_SIZE/2)]);
         }
+
+        blocks_to_process = blocks_to_process/2;
     }
 
     // print out hashed data
-    for (int j = 0; j < block_size_in_bytes/2; j++) {
+    for (int j = 0; j < BLOCK_SIZE/2; j++) {
         printf("%02x", hashed_data[j]);
     }
     printf("\n");
