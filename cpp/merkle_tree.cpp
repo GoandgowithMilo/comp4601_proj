b3862f566780a0584dd2a5d29a4604c28cb8c344
jamesrulz <jamesmilo42@gmail.com>
2025-07-31 17:20:12 +1000

    updated code logic

--- cpp/merkle_tree.cpp
+++ cpp/merkle_tree.cpp
@@ -1,50 +1,102 @@
-// Reads from this ap_uint 512 stream object
-// Store the result in some hash struct and return it (could we just replace it with a stream?)
 
 #include <ap_int.h>
 #include <hls_stream.h>
 
 #include "sha3_256.cpp"
 
-void merkle_tree_8(hls::stream<ap_uint<256>> &in , unsigned char output[32]) {
-    int i, j;
-    unsigned char input[64];
-    
-    unsigned char hashed_leaves[256];
+struct hash_t {
+    unsigned char to_hash[64];
+    unsigned char hashed[32];
+    int hash_layer;
+};
 
-    // We read in 4096 bits of data. For every 512 bits we read in we hash it and store it
-    for (i = 0; i < 8; i++) {
-        // We read in a pair of data
-        ap_uint<256> left  = in.read();
-        ap_uint<256> right = in.read();
-        ap_uint<512> combined;
+void input_manager(hls::stream<ap_uint<512>> &input, hls::stream<hash_t> &feedback, hls::stream<hash_t> &output) {
+    while (true) {
+        hash_t out;
 
-        // Check this works like I think it does...?
-        combined = (( (ap_uint<512>) right << 256 ) | (ap_uint<512>) left);
-        
-        // Convert to a byte array for sha3
-        for (j = 0; j < 64; j++) {
-            input[j] = combined.range(8 * (j + 1) - 1, 8 * j);
+        if (!feedback.empty()) {
+            hash_t in1 = feedback.read();
+            hash_t in2 = feedback.read();
+            out.hash_layer = in1.hash_layer;
+
+            for (int i = 0; i < 32; i++) {
+                out.to_hash[i]      = in1.hashed[i];
+                out.to_hash[i + 32] = in2.hashed[i];
+            }
+        } else {
+            ap_uint<512> in_val = input.read();
+            out.hash_layer = 0;
+            
+            for (int i = 0; i < 64; i++) {
+                out.to_hash[i] = in_val.range(8 * (i + 1) - 1, 8 * i);
+            }
         }
 
-        // Pass the 64 byte input and store the 32 byte hashed result
-        sha3_opt_256(input, &hashed_leaves[i*32]);
+        output.write(out);
     }
+}
 
-    // We read through the hashed array, pairing up data and rehashing it
-    unsigned char hashed_layer_1[128];
-    for (i = 0; i < 4; i++) {
-        // Pass the 64 byte input and store the 32 byte hashed result
-        sha3_opt_256(&hashed_leaves[i*64], &hashed_layer_1[i*32]);
+void sha3_pipeline(hls::stream<hash_t> &input, hls::stream<hash_t> &output) {
+    while (true) {
+        if (!input.empty()) {
+            hash_t data = input.read();
+
+            sha3_opt_256(data.to_hash, data.hashed);
+
+            output.write(data);
+        }
     }
+}
 
-    // Again
-    unsigned char hashed_layer_2[64];
-    for (i = 0; i < 2; i++) {
-        // Pass the 64 byte input and store the 32 byte hashed result
-        sha3_opt_256(&hashed_layer_1[i*64], &hashed_layer_2[i*32]);
+#define TREE_LAYERS 14
+void output_manager(hls::stream<hash_t> &input, hls::stream<hash_t> &feedback, hls::stream<ap_uint<256>> &output) {
+    static hash_t layers[TREE_LAYERS]; // For 16,384 leaves we have log_2(16384) = 14 layers of the tree
+    static bool layer_in_use[TREE_LAYERS];
+
+    while(true) {
+        if (!input.empty()) {
+            hash_t in = input.read();
+            int layer = in.hash_layer;
+
+            if (layer_in_use[layer]) { // we have an existing element at that layer so pair up and rehash
+                hash_t combined;
+                combined.hash_layer = layer + 1;
+
+                for (int i = 0; i < 32; i++) {
+                    combined.to_hash[i] = layers[layer].hashed[i];
+                    combined.to_hash[i + 32] = in.hashed[i];
+                }
+
+                layer_in_use[layer] = false;
+
+                if (combined.hash_layer < TREE_LAYERS) {
+                    feedback.write(combined);
+                } else { // we've hit the end and want to write back the resulting hash
+                    ap_uint<256> final_hash;
+                    for (int i = 0; i < 32; i++) {
+                        final_hash.range(8 * (i + 1) - 1, 8 * i) = combined.hashed[i];
+                    }
+                    output.write(final_hash);
+                }
+            } else { // we store the hash until its pair arrives
+                layers[layer] = in;
+                layer_in_use[layer] = true;
+            }
+
+        }
     }
+}
 
-    // We have 512 bits left at this point so we just write directly to the output buffer with the hash result
-    sha3_opt_256(hashed_layer_2, output);
+// Takes as input the data from the dataloader as a stream?
+void controller(hls::stream<ap_uint<512>> &in) {
+    hls::stream<hash_t> feedback; // output_manager => input_manager
+    hls::stream<hash_t> in_sha3; // input_manager => sha_3
+    hls::stream<hash_t> sha3_out; // sha_3 => output_manager
+    hls::stream<ap_uint<256>> result; // output_manager => ???
+
+    input_manager(in, feedback, in_sha3);
+    sha3_pipeline(in_sha3, out_sha3);
+    output_manager(sha3_out, feedback, result);
+
+    ap_uint<256> = result.read(); // What do we do with the output here? Is this blocking?
 }
