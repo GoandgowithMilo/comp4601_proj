e090eed74c30be76bf161fa0af14bd07e19102cb
jamesrulz <jamesmilo42@gmail.com>
2025-08-03 11:38:04 +1000

    WIP trying to debug deadlock

--- cpp/merkle_tree.cpp
+++ cpp/merkle_tree.cpp
@@ -5,6 +5,8 @@
 
 #include "sha3_256.h"
 
+#include <iostream>
+
 struct hash_t {
     unsigned char to_hash[64];
     unsigned char hashed[32];
@@ -14,36 +16,38 @@ struct hash_t {
 void input_manager(hls::stream<ap_uint<512>> &input, hls::stream<hash_t> &feedback, hls::stream<hash_t> &output) {
     hash_t out;
 
-    if (!feedback.empty()) {
+    if (feedback.size() >= 2) {
         hash_t in1 = feedback.read();
         hash_t in2 = feedback.read();
         out.hash_layer = in1.hash_layer;
 
         for (int i = 0; i < 32; i++) {
-            out.to_hash[i]      = in1.hashed[i];
+            out.to_hash[i] = in1.hashed[i];
             out.to_hash[i + 32] = in2.hashed[i];
         }
 
-    } else {
+        std::cout << out.hash_layer << std::endl;
+        output.write(out);
+
+    } else if (!input.empty()) {
         ap_uint<512> in_val = input.read();
         out.hash_layer = 0;
         
         for (int i = 0; i < 64; i++) {
             out.to_hash[i] = in_val.range(8 * (i + 1) - 1, 8 * i);
         }
+        
+        std::cout << out.hash_layer << std::endl;
+        output.write(out);
     }
-
-    output.write(out);
 }
 
 void sha3_pipeline(hls::stream<hash_t> &input, hls::stream<hash_t> &output) {
-    if (!input.empty()) {
-        hash_t data = input.read();
+    hash_t data = input.read();
 
-        sha3_opt_256(data.to_hash, data.hashed);
+    sha3_opt_256(data.to_hash, data.hashed);
 
-        output.write(data);
-    }
+    output.write(data);
 }
 
 #define TREE_LAYERS 3
@@ -51,35 +55,32 @@ void output_manager(hls::stream<hash_t> &input, hls::stream<hash_t> &feedback, h
     static hash_t layers[TREE_LAYERS]; // For 16,384 leaves we have log_2(16384) = 14 layers of the tree
     static bool layer_in_use[TREE_LAYERS];
 
-    if (!input.empty()) {
-        hash_t in = input.read();
-        int layer = in.hash_layer;
+    hash_t in = input.read();
+    int layer = in.hash_layer;
 
-        if (layer_in_use[layer]) { // we have an existing element at that layer so pair up and rehash
-            hash_t combined;
-            combined.hash_layer = layer + 1;
+    if (layer_in_use[layer]) { // we have an existing element at that layer so pair up and rehash
+        hash_t combined;
+        combined.hash_layer = layer + 1;
 
-            for (int i = 0; i < 32; i++) {
-                combined.to_hash[i] = layers[layer].hashed[i];
-                combined.to_hash[i + 32] = in.hashed[i];
-            }
-
-            layer_in_use[layer] = false;
-
-            if (combined.hash_layer < TREE_LAYERS) {
-                feedback.write(combined);
-            } else { // we've hit the end and want to write back the resulting hash
-                ap_uint<256> final_hash;
-                for (int i = 0; i < 32; i++) {
-                    final_hash.range(8 * (i + 1) - 1, 8 * i) = combined.hashed[i];
-                }
-                output.write(final_hash);
-            }
-        } else { // we store the hash until its pair arrives
-            layers[layer] = in;
-            layer_in_use[layer] = true;
+        for (int i = 0; i < 32; i++) {
+            combined.to_hash[i] = layers[layer].hashed[i];
+            combined.to_hash[i + 32] = in.hashed[i];
         }
 
+        layer_in_use[layer] = false;
+
+        if (combined.hash_layer < TREE_LAYERS) {
+            feedback.write(combined);
+        } else { // we've hit the end and want to write back the resulting hash
+            ap_uint<256> final_hash;
+            for (int i = 0; i < 32; i++) {
+                final_hash.range(8 * (i + 1) - 1, 8 * i) = combined.hashed[i];
+            }
+            output.write(final_hash);
+        }
+    } else { // we store the hash until its pair arrives
+        layers[layer] = in;
+        layer_in_use[layer] = true;
     }
 }
 
@@ -92,20 +93,3 @@ void merkle_tree(hls::stream<ap_uint<512>> &in, hls::stream<ap_uint<256>> &out) 
     hls::task t_sha3_pipeline(sha3_pipeline, in_sha3, sha3_out);
     hls::task t_output_manager(output_manager, sha3_out, feedback, out);
 }
-
-// Create a testbench for this and just use CSIM to verify output
-// To prevent deadlocks we want queues between copmonents to be large enough hence hls::stream<hash_t, 32>
-// Run CSIM, then run COSIM to check for deadlocks (size is ignored for csim)
-// verify and check no deadlocks!
-
-/* Need to come up with a way to produce an expected value... Could maybe use chatgpt to write a python program or modify
-my C++ program to do it? */
-
-/*
-    Create input and output streams
-    Generate dataset
-    create instance of controller with input and ouput streams
-    write data to input stream
-    read from the output stream
-
-*/
